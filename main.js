9ed300f7583ff1f4e706a5ec42045cdc6e3161f7
Namrata-Prajapati <namrataprajapati2300@gmail.com>
2024-03-14 01:24:32 +0530

    functions and keywords

--- main.js
+++ main.js
@@ -10,5 +10,83 @@ comments*/
 // arithematic, logical, assignment comparision
 // arithmatic:- +,-,*,/, %
 // assignment  :- =,  +=., -=, *=, /= 
-// logical:- ||, &&
+// logical:- || any of them are true, && all are true, !, 
 // comparison operator:- === compare the values along with datatype, == compares the values only
+ 
+// HOISTING is declaration of all the vars at the top
+// creating objects in Js
+const items ={
+    food: 1000,
+    drinks: 2000 
+};
+
+var cart =0;
+function addToCart(){ //without parameters
+    cart+= items.food;
+}
+console.log("without parameters:- ");
+addToCart();
+console.log("cart: ", cart);
+
+cart =0; 
+function addToCarPr(item){ //with parameters
+    cart+= item;
+}
+console.log("with parameters :- drinks and food")
+addToCarPr(items.drinks);
+addToCarPr(items.food);
+console.log("cart: ", cart);
+
+cart =0;
+console.log("return type function")
+function addCartReturn(item){
+    return cart+item;
+}
+
+cart = addCartReturn(items.food);
+console.log("cart: ", cart);
+
+console.log("Arrow functions latest in ES6");
+var addCart = (item) =>{
+    cart+= item;
+};
+console.log("with parameters :- drinks and food")
+addCart(items.drinks);
+addCart(items.food);
+console.log("cart: ", cart);
+
+console.log("See the difference between normal & arrow function");
+// NORMAL
+myFunction();
+function myFunction(){
+    console.log("NORMAL:- I can be declared after calling!!");
+}
+
+//ARROW FUNCTION
+const myArrFunction = () =>{
+    console.log("ARROW:- Hoisting doesn't work here basically arrow function will be called after defining it!!");
+}
+myArrFunction();
+
+//THIS keyword
+console.log("this:- this function will print the parent information of a page");
+
+function getThis(){
+    console.log(this);
+}
+
+const arrowGetThis = () => {
+    console.log(this);
+}
+
+getThis();
+arrowGetThis();
+
+const OurObject = {
+    name: "this keyword",
+    getThis : function(){console.log(this);},
+    arrowGetThis: () =>{console.log(this);}
+}
+
+OurObject.getThis(); // it gives object as parent this
+OurObject.arrowGetThis(); // it gives window as parent object
