298707987c28443c93faf3531b9dd4e19f3cfa86
Namrata-Prajapati <namrataprajapati2300@gmail.com>
2024-03-14 00:33:55 +0530

    Introduction of opertors

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("Hello this is my script tags");
+
+// single line comment
+/* multi 
+line 
+comments*/
+
+// if else gives boolean value in bits
+// operators
+// arithematic, logical, assignment comparision
+// arithmatic:- +,-,*,/, %
+// assignment  :- =,  +=., -=, *=, /= 
+// logical:- ||, &&
+// comparison operator:- === compare the values along with datatype, == compares the values only
